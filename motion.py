ceaf139acdf69125aac3ae23420d10ad71ae69a5
lewinms <lewin.michal@gmail.com>
2017-06-10 18:48:35 +0200

    added movement

--- motion.py
+++ motion.py
@@ -1,10 +1,5 @@
 from picamera.array import PiRGBArray
 from picamera import PiCamera
-<<<<<<< HEAD
-import time
-import cv2
-
-=======
 import RPi.GPIO as GPIO
 import time
 import datetime
@@ -14,14 +9,14 @@ class Movement:
     def __init__(self):
         GPIO.setmode(GPIO.BOARD)
         # Pins
-        self.LASER_PIN = 4
-        self.STEP_X = 17
-        self.DIR_X = 18
-        self.STEP_Y = 21
-        self.DIR_Y = 22
-        self.MOTOR_ENABLE = 25
-        self.END_X = 23
-        self.END_Y = 24
+        self.LASER_PIN = 38 #CHECKED
+        self.STEP_X = 35
+        self.DIR_X = 37
+        self.STEP_Y = 31
+        self.DIR_Y = 33
+        self.MOTOR_ENABLE = 29 #CHECKED
+        self.END_X = 32
+        self.END_Y = 36
 
         # Constant values
         self.STEP = 17.77
@@ -48,7 +43,6 @@ class Movement:
         GPIO.setup(self.MOTOR_ENABLE, GPIO.OUT, pull_up_down=GPIO.PUD_DOWN)
         GPIO.output(self.DIR_X, GPIO.LOW)
         GPIO.output(self.DIR_Y, GPIO.LOW)
-        GPIO.output(self.MOTOR_ENABLE, GPIO.LOW)
 
     def laser_on(self):
         GPIO.output(self.LASER_PIN, GPIO.HIGH)
@@ -73,12 +67,46 @@ class Movement:
         self.now_x = self.MIN_X
         self.now_y = self.MIN_Y
 
-    def move(self,angle_x,angle_y,scale = 1):
+    def move(self,angle_x,angle_y,multiplier  = 1):
+        positive_x = True
+        positive_y = True
         if angle_x >= 0.0:
             GPIO.output(self.DIR_X, GPIO.HIGH)
         else:
             GPIO.output(self.DIR_X, GPIO.LOW)
+            positive_x = False
+        if angle_y >= 0.0:
+            GPIO.output(self.DIR_Y, GPIO.HIGH)
+        else:
+            GPIO.output(self.DIR_Y, GPIO.LOW)
+            positive_y = False
+        angle_x = abs(angle_x)
+        angle_y = abs(angle_y)
 
+        while GPIO.input(self.END_X) == GPIO.HIGH or GPIO.input(self.END_Y) == GPIO.HIGH:
+            if angle_x >= 0.0:
+                GPIO.output(self.STEP_X, GPIO.HIGH)
+                if positive_x:
+                    self.now_x += 1.0/self.STEP
+                else:
+                    self.now_x -= 1.0/self.STEP
+                angle_x -= 1.0/self.STEP
+            if angle_y >= 0.0:
+                GPIO.output(self.STEP_Y, GPIO.HIGH)
+                if positive_y:
+                    self.now_y += 1.0 / self.STEP
+                else:
+                    self.now_y -= 1.0 / self.STEP
+                angle_y -= 1.0 / self.STEP
+            self.delay(self.DELAY*multiplier)
+            GPIO.output(self.STEP_X, GPIO.LOW)
+            GPIO.output(self.STEP_Y, GPIO.LOW)
+            self.delay(self.DELAY*multiplier)
+            if self.MAX_X <= self.now_x <= self.MIN_X or self.MAX_Y <= self.now_y <= self.MIN_Y:
+                return False
+            if angle_x <= 0.0 and angle_y <= 0.0:
+                break
+        return True
 
 
 class FPS:
@@ -120,12 +148,11 @@ class FPS:
         # compute the (approximate) frames per second
         return fps
 
-    def draw_text(self, frame, text, x, y, color=(200, 255, 155), thickness=2, size=1):
+    def draw_text(self, frame, text, x, y, color=(120, 255, 0), thickness=2, size=1):
         if x is not None and y is not None:
             cv2.putText(
                 frame, text, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, size, color, thickness)
 
->>>>>>> cdde3bd9bd332d52d49a280abe1e0b5fa6fcdf89
 
 def cameraControl(config):
     camera = PiCamera()
@@ -135,46 +162,24 @@ def cameraControl(config):
     rawCapture = PiRGBArray(camera, size=res)
     time.sleep(config["camera_warmup_time"])
     rememberFrame = None
-<<<<<<< HEAD
-    for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-        image = frame.array
-        rememberFrame,image = findMotion(image,rememberFrame,config)
-=======
     fpsCounter = FPS()
+    movement = Movement()
+    movement.calibrate()
     for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
         image = frame.array
         fps = fpsCounter.fps()
         fpsCounter.draw_text(image, "{:.3f}".format(fps), 30, 30)
-        # rememberFrame, image = findMotion(image, rememberFrame, config)
->>>>>>> cdde3bd9bd332d52d49a280abe1e0b5fa6fcdf89
+        # rememberFrame, image = findMotion(image, rememberFrame, config, movement)
         cv2.imshow("Primary", image)
         rawCapture.truncate(0)  # Clear capture for the next frame
         key = cv2.waitKey(1) & 0xFF
         if key == ord("q"):
             break
-<<<<<<< HEAD
-    cv2.destroyAllWindows()
-
-
-def findMotion(image,rememberFrame,config):
-    grayImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    grayImage = cv2.GaussianBlur(grayImage, (21, 21), 0)
-    if rememberFrame is None:
-        return grayImage,image
-    frameDelta = cv2.absdiff(rememberFrame, grayImage)
-    cv2.imshow("DIFF", frameDelta)
-
-    thresh = cv2.threshold(frameDelta, 10, 255, cv2.THRESH_BINARY)[1]
-
-    thresh = cv2.dilate(thresh, None, iterations=2)
-    (_, contours, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
-                                 cv2.CHAIN_APPROX_SIMPLE)
-=======
     GPIO.cleanup()
     cv2.destroyAllWindows()
 
 
-def findMotion(image, rememberFrame, config):
+def findMotion(image, rememberFrame, config, movement):
     grayImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     grayImage = cv2.GaussianBlur(grayImage, (21, 21), 0)
     if rememberFrame is None:
@@ -187,22 +192,32 @@ def findMotion(image, rememberFrame, config):
     thresh = cv2.dilate(thresh, None, iterations=2)
     (_, contours, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
                                         cv2.CHAIN_APPROX_SIMPLE)
->>>>>>> cdde3bd9bd332d52d49a280abe1e0b5fa6fcdf89
     figures = []
     for c in contours:
         if cv2.contourArea(c) < config["min_area"]:
             continue
-        (x, y, w, h) = cv2.boundingRect(c)
+        (pos_x, pos_y, width, height) = cv2.boundingRect(c)
         interserct = False
         if not interserct:
-<<<<<<< HEAD
-            figures.append((x,w,y,h))
-=======
-            figures.append((x, w, y, h))
->>>>>>> cdde3bd9bd332d52d49a280abe1e0b5fa6fcdf89
+            figures.append({'Pos_x': pos_x, 'Pos_y': pos_y, 'width': width, 'height': height})
     if len(figures) > 0:
         rememberFrame = None
+    target = None
     for fig in figures:
-        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        fig['delta_x'] = (fig['Pos_x'] + fig['width']/2) - (config["resolution"][0]/2)
+        fig['delta_y'] = (fig['Pos_y'] + fig['height'] / 2) - (config["resolution"][1]/2)
+        fig["distance"] = fig['delta_x']**2 + fig['delta_y']**2
+        if target is None:
+            target = fig
+        else:
+            if target["distance"] < fig["distance"]:
+                target = fig
+        cv2.rectangle(image, (fig['Pos_x'], fig['width']), (fig['Pos_x'] + fig['Pos_y'], fig['width']+ fig['height']), (0, 255, 0), 2)
+    if target is not None:
+        if target['delta_x'] > config['delta'] or target['delta_y'] > config['delta']:
+            if abs(target['delta_x']) < config['delta']: target['delta_x'] = 0.0
+            if abs(target['delta_y']) < config['delta']: target['delta_y'] = 0.0
+            inBoundries = movement.move()
+
 
     return rememberFrame, image
