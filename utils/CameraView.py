6aeabc617d39654ad20fecf722dfe104e68d415e
Michal Lewinski <lewin.michal@gmail.com>
2018-01-13 14:27:20 +0100

    Thread race

--- utils/CameraView.py
+++ utils/CameraView.py
@@ -1,12 +1,13 @@
 import cv2
 from threading import Thread
-
+from threading import RLock
 
 class CameraView:
     def __init__(self,config):
         self.viewDict = {}
         self.stopped = False
         self.config = config
+        self.lock = RLock()
 
     def start(self):
         if self.config['show_video'] is True:
@@ -15,14 +16,19 @@ class CameraView:
 
     def update(self):
         while True:
-            for key, image in self.viewDict.items():
+            self.lock.acquire()
+            cloneDict = self.viewDict.copy()
+            self.lock.release()
+            for key, image in cloneDict.items():
                 cv2.imshow(key, image)
             if self.stopped:
                 cv2.destroyAllWindows()
                 break
 
-    def show_image(self,name,image):
+    def show_image(self, name, image):
+        self.lock.acquire()
         self.viewDict[name] = image
-
+        self.lock.release()
+        
     def stop(self):
         self.stopped = True
