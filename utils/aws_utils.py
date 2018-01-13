2942510280d1325c0065cda43aaa0b60030e39e9
Kamil Piotrowski <kamil.piotrowski@nordcloud.com>
2018-01-13 18:05:13 +0100

    AWS_integration

---
+++ utils/aws_utils.py
@@ -0,0 +1,60 @@
+import boto3
+import datetime
+import json
+import threading
+import cv2
+import os
+
+TOPIC_ARN = 'arn:aws:sns:eu-central-1:972411866948:predator_sns'
+BUCKET = 'predator-rpi-bucket'
+IMAGE_UPLOAD_TIME_SEC = 5
+SNS_NOTIFICATION_TIME_SEC = 300
+BASE_PATH = '/tmp/'
+
+
+def send_notification(subject, message):
+    client = boto3.client('sns')
+    client.publish(
+        TopicArn=TOPIC_ARN,
+        Message=message,
+        Subject=subject
+    )
+
+
+def upload_image_s3(image_path, file_key):
+    s3 = boto3.resource('s3')
+    bucket = s3.Bucket(BUCKET)
+    bucket.upload_file(image_path, file_key)
+
+
+def save_and_upload_image(image):
+    path = "predator_capture_{}".format(str(datetime.datetime.now()))
+    os_path = "{}{}".format(BASE_PATH, path)
+    cv2.imwrite(os_path, image)
+    upload_image_s3(os_path, path)
+    delete_image(os_path)
+
+
+def delete_image(path):
+    os.remove(path)
+
+
+class AWSPredator:
+    def __init__(self):
+        self.last_s3_image_time = None
+        self.last_sns_time = None
+
+    def detected_movement(self, image):
+        now = datetime.datetime.now()
+        if self.last_s3_image_time is None or (now-self.last_s3_image_time).total_seconds() > IMAGE_UPLOAD_TIME_SEC:
+            self.last_s3_image_time = now
+            t = threading.Thread(target=save_and_upload_image, args=image)
+            t.start()
+
+        if self.last_sns_time is None or (now-self.last_sns_time).total_seconds() > SNS_NOTIFICATION_TIME_SEC:
+            self.last_sns_time = now
+            t = threading.Thread(target=send_notification, args=("Predator detected movement!", json.dumps({
+                "Message": "Predator detected movement",
+                "Time": str(now),
+            })))
+            t.start()
--- utils/tracking.py
+++ utils/tracking.py
@@ -6,11 +6,14 @@ from utils.fps import FPS
 from utils.motion import Movement
 from utils.cameraTest import findMotion
 from utils.CameraRawCapture import PiVideoStream
+from utils.aws_utils import AWSPredator
+
 
 def camera_control(config):
     # Pi configuration and calibration
     movement = Movement()
     movement.calibrate()
+    aws_predator = AWSPredator()
     video_stream = PiVideoStream(config).start()
     time.sleep(config["camera_warmup_time"])
     remember_frame = None
@@ -21,10 +24,10 @@ def camera_control(config):
     found = False
     while True:
         image = video_stream.read_frame()
-        if(current_count == 0):
+        if (current_count == 0):
             # remember_frame = None
             summary = None
-        remember_frame, target, summary = findMotion(image, remember_frame, config,current_count,frame_count,summary)
+        remember_frame, target, summary = findMotion(image, remember_frame, config, current_count, frame_count, summary)
         current_count += 1
         # if remember_frame is None:
         #     remember_frame = new_frame
@@ -33,10 +36,11 @@ def camera_control(config):
             start_time = None
             summary = None
             current_count = 0
-           #movement.laser_on()
+            # movement.laser_on()
+            aws_predator.detected_movement(image)
             inBoundries = movement.move(target['angle_x'], target['angle_y'], multiplier=10)
             time.sleep(0.2)
-           #movement.laser_off()
+            # movement.laser_off()
             rememberFrame = None
             if inBoundries is False:
                 movement.center()
@@ -48,16 +52,16 @@ def camera_control(config):
             end_time = datetime.datetime.now()
             if (end_time - start_time).total_seconds() >= 1.5:
                 print("Tracking failure detected")
-                #cv2.putText(image, "Tracking failure detected", (100, 80), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255),2)
+                # cv2.putText(image, "Tracking failure detected", (100, 80), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255),2)
             if (end_time - start_time).total_seconds() >= 5.0:
                 summary = None
                 current_count = 0
                 start_time = None
                 remember_frame = None
                 found = False
-        current_count = current_count % (frame_count+1)
+        current_count = current_count % (frame_count + 1)
         if config['show_video'] is True:
-            #draw_text(image, "{:.3f}".format(fps), 30, 30)
+            # draw_text(image, "{:.3f}".format(fps), 30, 30)
             cv2.imshow("Primary", image)
         key = cv2.waitKey(1) & 0xFF
         if key == ord("q"):
