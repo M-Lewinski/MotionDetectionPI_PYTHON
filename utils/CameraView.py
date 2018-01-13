d8d2329691db081cce5b6645aaf845c7c9acf6d1
Michal Lewinski <lewin.michal@gmail.com>
2018-01-13 15:06:09 +0100

    Camera view no longer in other thread

--- utils/CameraView.py
+++ utils/CameraView.py
@@ -1,24 +1,18 @@
 import cv2
 from threading import Thread
-from threading import RLock
 
 class CameraView:
     def __init__(self,config):
         self.viewDict = {}
         self.stopped = False
         self.config = config
-        self.lock = RLock()
 
     def start(self):
-        if self.config['show_video'] is True:
-            Thread(target=self.update, args=()).start()
         return self
 
     def update(self):
         while True:
-            self.lock.acquire()
             cloneDict = self.viewDict.copy()
-            self.lock.release()
             for key, image in cloneDict.items():
                 cv2.imshow(key, image)
             if self.stopped:
@@ -26,9 +20,8 @@ class CameraView:
                 break
 
     def show_image(self, name, image):
-        self.lock.acquire()
-        self.viewDict[name] = image
-        self.lock.release()
-        
+        if self.config["show_video"] is True:
+            cv2.imshow(name,image)
+
     def stop(self):
         self.stopped = True
